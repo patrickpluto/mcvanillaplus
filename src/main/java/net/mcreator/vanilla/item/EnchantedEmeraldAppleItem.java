881b02ea3d7afe07cf86200dbf0938d7302566e9
OMEN 870-296nz <OMEN 870-296nz@DESKTOP-KTDPR7B>
2020-05-23 22:20:34 +0200

    added emerald apples and craftable enchanted golden apples

---
+++ src/main/java/net/mcreator/vanilla/item/EnchantedEmeraldAppleItem.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.vanilla.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.LivingEntity;
+
+import net.mcreator.vanilla.procedures.EnchantedEmeraldAppleFoodEatenProcedure;
+import net.mcreator.vanilla.VanillaModElements;
+
+@VanillaModElements.ModElement.Tag
+public class EnchantedEmeraldAppleItem extends VanillaModElements.ModElement {
+	@ObjectHolder("vanilla_:enchanted_emerald_apple")
+	public static final Item block = null;
+	public EnchantedEmeraldAppleItem(VanillaModElements instance) {
+		super(instance, 18);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64)
+					.food((new Food.Builder()).hunger(20).saturation(20f).setAlwaysEdible().build()));
+			setRegistryName("enchanted_emerald_apple");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack par1ItemStack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemStack, World world, LivingEntity entity) {
+			ItemStack retval = super.onItemUseFinish(itemStack, world, entity);
+			int x = (int) entity.getPosX();
+			int y = (int) entity.getPosY();
+			int z = (int) entity.getPosZ();
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("entity", entity);
+				EnchantedEmeraldAppleFoodEatenProcedure.executeProcedure($_dependencies);
+			}
+			return retval;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/vanilla/procedures/DiamondAppleEatenProcedure.java
@@ -0,0 +1,35 @@
+package net.mcreator.vanilla.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.vanilla.VanillaModElements;
+
+@VanillaModElements.ModElement.Tag
+public class DiamondAppleEatenProcedure extends VanillaModElements.ModElement {
+	public DiamondAppleEatenProcedure(VanillaModElements instance) {
+		super(instance, 20);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure DiamondAppleEaten!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 300, (int) 3));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.ABSORPTION, (int) 300, (int) 3));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 300, (int) 3));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 300, (int) 3));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, (int) 300, (int) 3));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.STRENGTH, (int) 300, (int) 3));
+	}
+}
